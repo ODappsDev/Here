0ac6ba2a563e13297f1735d1722497bf8ee546ae
Daniel Stolero <Daniel.Stolero@Odapps.co.il>
2018-10-13 23:30:11 +0300

    no message

---
+++ app/src/main/java/com/here/danielstolero/here/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.here.danielstolero.here.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.here.danielstolero.here.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
